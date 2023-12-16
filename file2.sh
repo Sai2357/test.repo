70b5324e1b9270c7d534eab386cf32f9bec73aa4
Sai2357 <you@example.com>
2023-12-16 21:56:04 +0530

    as per bug

--- file2.sh
+++ file2.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 Person1=$1
-person2=$2
+Person2=$2
 
-echo "$person1 :Hi $person2,how are you"
-echo "$person2 :Hello $person1 am fine"
-echo "$person1 :now where are you staying $person2"
+echo "$Person1 :Hi $Person2,how are you"
+echo "$Person2 :Hello $Person1 am fine"
+echo "$Person1 :now where are you staying $Person2"
 
 
