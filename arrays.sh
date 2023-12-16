1e1f5dccf22aead78d67bbecf9dfb8d47283df66
Sai2357 <you@example.com>
2023-12-16 22:56:35 +0530

    as per bug

--- arrays.sh
+++ arrays.sh
@@ -3,6 +3,6 @@
 NAME=("PAVAN KALYAN" "CM" "AP")
 
 echo "first name is ${NAME[0]}"
-echo "first name is ${NAME[1]}"
-echo "first name is ${NAME[2]}"
+echo "Second name is ${NAME[1]}"
+echo "Third name is ${NAME[2]}"
 
