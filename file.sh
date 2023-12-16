8d788abf1947b5a06cddaa7217e3fd6d4256f473
Sai2357 <you@example.com>
2023-12-16 22:18:16 +0530

    as per bug

--- file.sh
+++ file.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
+logfile="/tmp/logs.log"
 PERSON1=pavan
 PERSON2=kiran
 
 
-echo "$PERSON1: HI,$PERSON2 "
-echo "$PERSON2: hELLO ,$PERSON1,GOOD MORNING"
+echo "$PERSON1: HI,$PERSON2 " &>>$logfile
+echo "$PERSON2: hELLO ,$PERSON1,GOOD MORNING" &>>$logfile
