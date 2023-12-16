943ad66d4c4c8cd5c39297762c32536040e0e051
Sai2357 <you@example.com>
2023-12-16 22:10:39 +0530

    as per bug

--- file1.sh
+++ file1.sh
@@ -3,8 +3,8 @@
 Person1=ramesh
 person2=suresh
 
-echo "$person1 :Hi $person2,how are you"
-echo "$person2 :Hello $person1 am fine"
-echo "$person1 :now where are you staying $person2"
+echo "$Person1 :Hi $person2,how are you"
+echo "$person2 :Hello $Person1 am fine"
+echo "$Person1 :now where are you staying $person2"
 
 
