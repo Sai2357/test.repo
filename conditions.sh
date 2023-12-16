80242ff2a4e7378fa03a4b6dcc45d1f16b92dc8d
Sai2357 <you@example.com>
2023-12-16 23:41:37 +0530

    as per bug

--- conditions.sh
+++ conditions.sh
@@ -3,7 +3,9 @@
 NUMBER=$1
 
 if( $NUMBER -gt 100 )
-echo "$NUMBER is  not greater than 100"
+then
+
+   echo "$NUMBER is  not greater than 100"
 else
-echo "$NUMBER is greaterthan 100"
+   echo "$NUMBER is greaterthan 100"
 fi
