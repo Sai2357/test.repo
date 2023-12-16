43c84100b45636cd02f4065301e9712fddb76d4d
Sai2357 <you@example.com>
2023-12-16 17:19:26 +0530

    as per bug

---
+++ read.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "please enter your username"
+read -s username  #read will fetch the username dynamically from user
+
+echo "please enter your password"
+read -s password
+
+echo "username is $username and password is $password"
