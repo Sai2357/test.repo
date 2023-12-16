0a09a87e879550891e2849f108825cb3aa406fe6
Sai2357 <you@example.com>
2023-12-16 16:42:43 +0530

    as per bug

---
+++ file1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1=ramesh
+person2=suresh
+
+echo "$person1 :Hi $person2,how are you
+echo "$person2 :Hello $person1 am fine
+echo "$person1 :now where are you staying $person2:
+
