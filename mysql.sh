620d67884de16f81d1cfafd0fe26786768140c8e
Sai2357 <you@example.com>
2023-12-18 22:32:51 +0530

    install mysql

--- mysql.sh
+++ mysql.sh
@@ -4,10 +4,22 @@ ID=$(id -u)
 
 if [ $ID -ne 0 ]
 
-echo "error :pls run with root user"
-exit 1
+    echo "error :pls run with root user"
+    exit 1
 then 
-echo "ur root"
+    echo "ur root"
 
 fi
 yum install mysql
+
+if [ $? -ne 0 ]
+
+    echo "installation of mysql is not sucess"
+
+then
+    echo "installation of mysql is sucess"
+
+fi
+
+ 
+
